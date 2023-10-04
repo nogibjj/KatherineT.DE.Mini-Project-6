f3f76b7319b3f55c5cefdd47a102dbd8f4069058
katherinetian540 <143833511+katherinetian540@users.noreply.github.com>
2023-10-04 14:30:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -30,24 +30,23 @@ def handle_arguments(args):
 
     args = parser.parse_args(args[:1])
     print(args.action)
-
     if args.action == "update_record":
         parser.add_argument("team")
         parser.add_argument("group")
-        parser.add_argument("confederation")
-        parser.add_argument("population_share")
-        parser.add_argument("tv_audience_share")
-        parser.add_argument("gdp_weighted_share")
+        parser.add_argument("spi")
+        parser.add_argument("global_o")
+        parser.add_argument("global_d")
+        parser.add_argument("sim_wins")
 
     if args.action == "create_record":
-        parser.add_argument("country")
-        parser.add_argument("confederation")
-        parser.add_argument("population_share")
-        parser.add_argument("tv_audience_share")
-        parser.add_argument("gdp_weighted_share")
+        parser.add_argument("group")
+        parser.add_argument("spi")
+        parser.add_argument("global_o")
+        parser.add_argument("global_d")
+        parser.add_argument("sim_wins")
 
     if args.action == "delete_record":
-        parser.add_argument("record_id", type=int)
+        parser.add_argument("team", type=TEXT)
 
     return parser.parse_args(sys.argv[1:])
 
@@ -66,24 +65,24 @@ def main():
 
     elif args.action == "update_record":
         update_record(
-            args.record_id,
-            args.country,
-            args.confederation,
-            args.population_share,
-            args.tv_audience_share,
-            args.gdp_weighted_share,
+            args.team,
+            args.group,
+            args.spi,
+            args.global_o,
+            args.global_d,
+            args.sim_wins,
         )
 
     elif args.action == "delete_record":
-        delete_record(args.record_id)
+        delete_record(args.team)
 
     elif args.action == "create_record":
         create_record(
-            args.country,
-            args.confederation,
-            args.population_share,
-            args.tv_audience_share,
-            args.gdp_weighted_share,
+            args.group,
+            args.spi,
+            args.global_o,
+            args.global_d,
+            args.sim_wins,
         )
 
     elif args.action == "read_data":
