2841ccb9e88660d78e91bc787c3e9abd8355b584
katherinetian540 <143833511+katherinetian540@users.noreply.github.com>
2023-10-04 14:00:01 -0400

    Update query.py

--- mylib/query.py
+++ mylib/query.py
@@ -2,15 +2,112 @@
 
 import sqlite3
 
+# Define a global variable for the log file
+LOG_FILE = "query_log.md"
 
-def query():
-    """Query the database for the top 5 rows of the GroceryDB table"""
-    conn = sqlite3.connect("GroceryDB.db")
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM GroceryDB")
-    print("Top 5 rows of the GroceryDB table:")
-    print(cursor.fetchall())
+
+def log_query(query):
+    """adds to a query markdown file"""
+    with open(LOG_FILE, "a") as file:
+        file.write(f"```sql\n{query}\n```\n\n")
+
+
+def create_record(
+    country, confederation, population_share, tv_audience_share, gdp_weighted_share
+):
+    """create example query"""
+    conn = sqlite3.connect("fifaDB.db")
+    c = conn.cursor()
+    c.execute(
+        """
+        INSERT INTO fifaDB 
+        (country, 
+        confederation, 
+        population_share,
+        tv_audience_share, 
+        gdp_weighted_share) 
+        VALUES (?, ?, ?, ?, ?)
+        """,
+        (country, 
+         confederation, 
+         population_share, 
+         tv_audience_share, 
+         gdp_weighted_share),
+    )
+    conn.commit()
     conn.close()
-    return "Success"
+
+    log_query(
+        f"""INSERT INTO fifaDB VALUES (
+                {country}, 
+                {confederation},
+                {population_share},
+                {tv_audience_share},
+                {gdp_weighted_share});"""
+    )
 
 
+def read_data():
+    """read data"""
+    conn = sqlite3.connect("fifaDB.db")
+    c = conn.cursor()
+    c.execute("SELECT * FROM fifaDB")
+    data = c.fetchall()
+    log_query("SELECT * FROM fifaDB;")
+    return data
+
+
+def update_record(
+            record_id,
+            country, 
+            confederation, 
+            population_share, 
+            tv_audience_share, 
+            gdp_weighted_share
+):
+    """update example query"""
+    conn = sqlite3.connect("fifaDB.db")
+    c = conn.cursor()
+    c.execute(
+        """
+        UPDATE fifaDB 
+        SET country=?, 
+        confederation=?, 
+        population_share=?, 
+        tv_audience_share=?, 
+        gdp_weighted_share=?
+        WHERE id=?
+        """,
+        (
+            country, 
+            confederation, 
+            population_share, 
+            tv_audience_share, 
+            gdp_weighted_share,
+            record_id
+        ),
+    )
+    conn.commit()
+    conn.close()
+
+    log_query(
+        f"""UPDATE fifaDB SET 
+        country={country}, 
+        confederation=
+        {confederation},
+        population_share={population_share}, 
+        tv_audience_share={tv_audience_share}, 
+        gdp_weighted_share={gdp_weighted_share},
+        WHERE id={record_id};"""
+    )
+
+
+def delete_record(record_id):
+    """delete example query"""
+    conn = sqlite3.connect("fifaDB.db")
+    c = conn.cursor()
+    c.execute("DELETE FROM fifaDB WHERE id=?", (record_id,))
+    conn.commit()
+    conn.close()
+
+    log_query(f"DELETE FROM fifaDB WHERE id={record_id};")
