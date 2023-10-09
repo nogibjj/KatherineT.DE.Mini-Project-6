c368a5e53c70a8e44e85ab687554f854329cf790
katherinetian540 <143833511+katherinetian540@users.noreply.github.com>
2023-10-09 15:31:35 -0400

    Update transform_load.py

--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -1,45 +1,49 @@
 """
-Transforms and Loads data into the local SQLite3 database
-
+Transforms and Loads data into Azure Databricks
 """
-import sqlite3
-import csv
+import os
+from databricks import sql
+import pandas as pd
+from dotenv import load_dotenv
 
 
-# load the csv file and insert into a new sqlite3 database
+# load the csv file and insert into databricks
 def load(dataset="data/births.csv"):
-    """Transforms and Loads data into the local SQLite3 database"""
-    payload = csv.reader(open(dataset, newline=""), delimiter=",")
-    # skips the header of csv
-    next(payload)
-    conn = sqlite3.connect("birthDB.db")
-    c = conn.cursor()
-    c.execute("DROP TABLE IF EXISTS birthDB")
-    c.execute(
-        """
-        CREATE TABLE birthDB (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            year INTEGER,
-            month INTEGER,
-            date_of_month INTEGER,
-            day_of_week INTEGER,
-            births INTEGER
-        )
-    """
-    )
-    # insert
-    c.executemany(
-        """
-        INSERT INTO birthDB(
-            year, 
-            month,
-            date_of_month, 
-            day_of_week,
-            births
-            ) 
-            VALUES (?,?, ?, ?, ?)""",
-        payload,
-    )
-    conn.commit()
-    conn.close()
-    return "extract.db"
+    """Transforms and Loads data into the local databricks database"""
+    df = pd.read_csv(dataset, delimiter=",", skiprows=1)
+    load_dotenv()
+    server_h = os.getenv("SERVER_HOSTNAME")
+    access_token = os.getenv("ACCESS_TOKEN")
+    http_path = os.getenv("HTTP_PATH")
+    with sql.connect(
+        server_hostname=server_h,
+        http_path=http_path,
+        access_token=access_token,
+    ) as connection:
+        c = connection.cursor()
+        # INSERT TAKES TOO LONG
+        # c.execute("DROP TABLE IF EXISTS ServeTimesDB")
+        c.execute("SHOW TABLES FROM default LIKE 'serve*'")
+        result = c.fetchall()
+        # takes too long so not dropping anymore
+        # c.execute("DROP TABLE IF EXISTS EventTimesDB")
+        if not result:
+            c.execute(
+                """
+                CREATE TABLE IF NOT EXISTS birthDB (
+                    id int,
+                    year int,
+                    month int,
+                    date_of_month int,
+                    day_of_week int,
+                    births int
+                )
+            """
+            )
+            # insert
+            for _, row in df.iterrows():
+                convert = (_,) + tuple(row)
+                c.execute(f"INSERT INTO birthdb VALUES {convert}")
+        c.close()
+
+    return "success"
